f189ba89b0b804f2f6782e9150c51b98ce2a2bd9
jina-jun <rlanij13@gmail.com>
2021-06-07 21:35:07 +0900

    search bar 일부 해결

--- res/js/common.js
+++ res/js/common.js
@@ -1,6 +1,6 @@
 $(function(){
-    // search bar
-    $('.search_bar input').focus(function(){
+    // search bar - focused
+    $('.search_inp').focus(function(){
         $('.placeholder').css('display', 'none');
         $('header').addClass('active');
         $('.dropdown').css('display', 'block');
@@ -10,27 +10,32 @@ $(function(){
             addRecent();
             $('#ui-id-1').css('display', 'none');
         });
-        $('.search_bar input').keyup(function(e){
+        $('.search_inp').keyup(function(e){
             if(e.keyCode === 13){
                 addRecent();
                 $('#ui-id-1').css('display', 'none');
             }
+            if(e.keyCode === 27){
+                inpBlur();
+            }
+        });
+        $('header').click(function(){
+            $('.search_inp').focus();
         });
         // delete all tags
         delAllTags();
-        // searh matching
-        var list = [
-            '제로', '제로웨이스트', '제로페이', '제로콜라', '제로플라스틱', '플라스틱', '플라스틱프리',
-            '재사용', '용기내', '분리배출', '리사이클링', '재활용', '업사이클링', '리필', '리필스테이션', '리폼'
-        ]
-        $('.search_bar input').autocomplete({
-            source: list
-        });
     });
-    $('.search_bar input').blur(function(){
-        $('.placeholder').css('display', 'block');
-        $('header').removeClass('active');
-        $('.dropdown').css('display', 'none');
+    // searh matching
+    var list = [
+        '제로', '제로웨이스트', '제로페이', '제로콜라', '제로플라스틱', '플라스틱', '플라스틱프리',
+        '재사용', '용기내', '분리배출', '리사이클링', '재활용', '업사이클링', '리필', '리필스테이션', '리폼'
+    ]
+    $('.search_inp').autocomplete({
+        source: list
+    });
+    // search bar - unfocused
+    $('section').click(function(){
+        inpBlur();
     });
     // modal login
     $('.btn_login').click(function(){
@@ -40,16 +45,27 @@ $(function(){
         $('.modal_login').css('display', 'none');
     });
 });
+function inpBlur(){
+    $('.placeholder').css('display', 'block');
+        $('header').removeClass('active');
+        $('input').val('');
+        $('#ui-id-1, .dropdown').css('display', 'none');
+        $('.search_inp').blur();
+        $('.recent .tag').removeClass('sel b');
+        $('.suggest .tag').removeClass('sel');
+        $('.recent .tag').addClass('s');
+        return;
+}
 function delAllTags(){
     $('.del_all').click(function(){
         $('.recent .tag').remove();
-        $('.search_bar input').focus();
+        $('.search_inp').focus();
     });
 }
 function addRecent(){
-    if($('.search_bar input').val() === '') return;
-    $('.recent').append(`<span class="tag s">${$('.search_bar input').val()}</span>`);
-    $('.search_bar input').val('').focus();
+    if($('.search_inp').val() === '') return;
+    $('.recent').append(`<span class="tag s">${$('.search_inp').val()}</span>`);
+    $('.search_inp').val('').focus();
     if($('.recent span').length > 5)
         $('.recent span:first-child').remove();
     $('.recent .tag').removeClass('sel b');
@@ -67,15 +83,15 @@ function selTags(){
     $('.dropdown .tag').on('click mouseup', function(){
         var inputText = $(this).text();
         if($(this).parent().hasClass('recent')){
-            $('.search_bar input').val(inputText);
-            $('.search_bar input').focus();
+            $('.search_inp').val(inputText);
+            $('.search_inp').focus();
             $('.suggest .tag').removeClass('sel');
             $('.recent .tag').removeClass('sel b');
             $('.recent .tag').addClass('s');
             $(this).toggleClass('s');
         } else if($(this).parent().hasClass('suggest')){
-            $('.search_bar input').val(inputText.substr(1));
-            $('.search_bar input').focus();
+            $('.search_inp').val(inputText.substr(1));
+            $('.search_inp').focus();
             $('.recent .tag').removeClass('sel b');
             $('.recent .tag').addClass('s');
             $('.suggest .tag').removeClass('sel');
